4ef7500c3e6d5ee3c6a5b0abfd55cbdb074daf68
laryea <kalexgun@gmail.com>
2025-03-06 17:48:31 +0000

    Day 1 assignment on Variables and Data Types

---
+++ day-1/datatypes.js
@@ -0,0 +1,19 @@
+let myName = "Godwin";
+
+let myAge = 27;
+
+let sTechie = true;
+
+let zerebel
+
+let mister = null;
+
+typeof myName; // string
+
+typeof myAge; // number
+
+typeof sTechie; // boolean
+
+typeof zerebel; // undefined
+
+typeof mister; // object
--- day-1/variables.js
+++ day-1/variables.js
@@ -1,1 +1,62 @@
+// Comments can make code readable
+
+// Welcome to JavaScript
+
+/* 
+  Comments can make code readable, 
+    easy to reuse and informative 
+*/
+
+// Variables with different data types
+
+const myName = "Godwin";
+
+const day = 27;
+
+let sTechie = true;
+
+let zerebel
+
+// Four Variables declared below with no values assigned to them
+
+let kofi
+
+let location
+
+let qualification
+
+let dressCode
+
+// Four variables declared below with values assigned to them
+
+const myLocation = "Lartebiokorshie";
+
+const myQualification = "BSc. Computer Science";
+
+let myDressCode = "Casual";
+
+let myGender = "Male";
+
+// Variables with my name, marital status, etc
+
+const firstName = "Godwin";
+
+const lastName = "Laryea";
+
+let maritalStatus = "Single";
+
+let country = "Ghana";
+
+let age = 27;
+
+// Variables containing my age and your age
+
+let myAge = 27;
+
+let yourAge = 30;
+
+console.log(myAge, yourAge);
+
+
+
 
