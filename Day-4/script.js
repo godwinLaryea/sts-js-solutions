6c5fee6f879fead21193e66116e24ff69997f940
laryea <kalexgun@gmail.com>
2025-03-17 13:18:33 +0000

    Day 4 level 1 exercise on ternary, if/else and modulo

--- Day-4/script.js
+++ Day-4/script.js
@@ -1,1 +1,46 @@
+// Q1
+let userAge = prompt("Enter your age");
+
+if(userAge >= 18){
+  console.log("You are old enough to drive");
+}else if(userAge < 18){
+console.log("You are left with " + (18 - userAge) + " years to drive");
+}else{
+  console.log("You should enter a number dawg");
+};
+
+//Q2
+let myAge = prompt("Enter my age"),
+yourAge = prompt("Enter your age");
+
+if (myAge > yourAge){
+  console.log("I am " + (myAge-yourAge) + " years older than you");
+}else if(myAge < yourAge){
+  console.log("You are " + (yourAge-myAge) + " years older than me");
+}else if(myAge == yourAge){
+  console.log("We are of the same age");
+}else{
+  console.log("Just enter numbers dawg");
+};
+
+// Q3
+let a = 4,
+b = 3;
+
+if(a > b){
+  console.log(`${a} is greater than ${b}`);
+}else{
+  console.log(`${b} is greater than ${a}`);
+};
+
+(a > b) ? console.log(a + " is greater than "  + b) : console.log( b + " is greater than " + a);
+
+// Q4
+let evenNumberChecker = prompt("Enter any number");
+
+if(evenNumberChecker % 2 == 0){
+  console.log(`${evenNumberChecker} is an even number`);
+}else{
+  console.log(`${evenNumberChecker} is an odd number`);
+};
 
